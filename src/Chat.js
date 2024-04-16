97164a10f8325d54bdddb4b56e804acbea553b5d
Rion Kelmendi <rkelmendi@jacobs-university.de>
2024-04-16 16:28:45 +0200

    added mongodb

--- src/Chat.js
+++ src/Chat.js
@@ -1,64 +1,84 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
-function Chat({socket,username,room}) {
-    const [currentMessage,setCurrentMessage] = useState("");
-    const [messageList,setMessageList] = useState([]);
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
-    const sendMessage = async () => {
-        if(currentMessage !== ""){
-            const messageData = {
-                room:room,
-                author: username,
-                message: currentMessage,
-                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
-            };
-        
-            await socket.emit("send_message",messageData);
-            setMessageList((list) => [...list, messageData]);
-        }
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
+  };
 
-    useEffect(() => {   
-        socket.on("receive_message",(data)=>{
-            console.log(data);
-            setMessageList((list) => [...list, data]);
-        });
+  useEffect(() => {
+    const receiveMessage = (data) => {
+      setMessageList((list) => [...list, data]);
+    };
+  
+    socket.on("receive_message", receiveMessage);
+  
+    // Clean up the event listener when the component is unmounted or when `socket` changes
+    return () => {
+      socket.off("receive_message", receiveMessage);
+    };
+  }, [socket]);
 
-        // Clean up the event listener when the component is unmounted
-        return () => {
-            socket.off("receive_message");
-        };
-    },[socket]);
-
-    return (
-        <div className="chat-window">
-            <div className="chat-header">
-                <p>Live chat </p>
-            </div>
-            <div className="chat-body">
-                {messageList.map((messageContent)=>{
-                    return <div className="message"  id={username==messageContent.author ? "you" :"other"}>
-                        <div> 
-                            <div className="message-content">
-                                <p>{messageContent.message}</p>
-                                </div>
-                            <div className="message-meta">
-                                <p id="author">{messageContent.author}</p>
-                                <span id="time">{messageContent.time}</span>
-                            </div>
-                         </div>
-                        </div>
-                }
-                )}
-            </div>
-            <div className="chat-footer"></div>
-            <input type="text" placeholder="Type a message..." onChange={(event) => setCurrentMessage(event.target.value)} />
-            <button onClick={sendMessage}>
-            &#9658;
-            </button>
-        </div>
-    );
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+          }}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
